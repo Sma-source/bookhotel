f5003b65373180ebc575473c4688b95db392d18b
Sma-source <rabhi.smain@gmail.com>
2023-06-14 19:20:04 +0200

    update ui

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -7,7 +7,18 @@ import { BsArrowsFullscreen, BsPeople } from "react-icons/bs";
 const Room = ({ room }) => {
   // destructure room
   const { id, name, image, size, maxPerson, description, price } = room;
-  return <div>Room</div>;
+  return (
+    <div className="bg-white shadow-2xl min-h-[500px] group">
+      {/* img */}
+      <div className="overflow-hidden">
+        <img
+          className="group-hover:scale-110 transition-all duration-300 w-full"
+          src={image}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Room;
