cfa5c782ccda79fdc1413fbb1189692857544087
Sma-source <rabhi.smain@gmail.com>
2023-06-14 18:47:52 +0200

    make import

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+//link
+import { Link } from "react-router-dom";
+// icons
+import { BsArrowsFullscreen, BsPeople } from "react-icons/bs";
 
 const Room = () => {
   return <div>Room</div>;
