5c7689fecabbbca3b3f0ae5dc0c515fe29e2d587
Sma-source <rabhi.smain@gmail.com>
2023-06-14 20:46:50 +0200

    add ui

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -17,6 +17,51 @@ const Room = ({ room }) => {
           alt=""
         />
       </div>
+      {/* details */}
+      <div
+        className="bg-white shadow-lg max-w-[300px] mx-auto h-[60px]
+      -translate-y-1/2 flex justify-center items-center uppercase text-xs font-tertiary tracking-[1px]
+      font-semibold"
+      >
+        <div className="flex justify-between w-[90%]">
+          {/* size */}
+          <div className="flex items-center gap-x-2">
+            <div className="text-accent">
+              <BsArrowsFullscreen className="text-[14px]" />
+            </div>
+            <div className="flex gap-x-1">
+              <div>Size</div>
+              <div>{size}m²</div>
+            </div>
+          </div>
+          {/* room capacity */}
+          <div className="flex items-center gap-x-2">
+            <div className="text-accent">
+              <BsPeople className="text-[17px]" />
+            </div>
+            <div className="flex gap-x-1">
+              <div>Max people</div>
+              <div>{maxPerson}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* name & description */}
+      <div className="text-center">
+        <Link to={`/room/${id}`}>
+          <h3 className="h3">{name}</h3>
+        </Link>
+        <p className="max-w-[300px] mx-auto mb-3 lg:mb-6">
+          {description.slice(0, 56)}
+        </p>
+      </div>
+      {/* btn */}
+      <Link
+        to={`/room/${id}`}
+        className="btn btn-secondary btn-sm text-xs max-w-[250px] mx-auto text-center"
+      >
+        Book now from ${price}
+      </Link>
     </div>
   );
 };
