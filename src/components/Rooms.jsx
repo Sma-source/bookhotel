4e8aa8f68f49fa59637795088e236cc1be1c7528
Sma-source <rabhi.smain@gmail.com>
2023-06-14 18:31:51 +0200

    map throw romms data context

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+// context
+import { RoomContext } from "../context/RoomContext";
+// components
+import Room from "../components/Room";
 
 const Rooms = () => {
-  return <div>Rooms</div>;
+  const { rooms } = useContext(RoomContext);
+  return (
+    <section className="py-24">
+      <div className="container mx-auto lg:px-0">
+        {/* grid */}
+        <div>
+          {rooms.map((room) => {
+            return <Room key={room.id} room={room} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Rooms;
