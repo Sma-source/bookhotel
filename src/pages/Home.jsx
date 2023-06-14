19753f5bc38569f3314bf554511815d4f044f832
Sma-source <rabhi.smain@gmail.com>
2023-06-14 20:59:04 +0200

    import & display Bookform component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 // components
 import Rooms from "../components/Rooms";
+import BookForm from "../components/BookForm";
 
 const Home = () => {
   return (
     <>
+      <BookForm />
       <Rooms />
     </>
   );
