db38cdae1666a33a530b0f1442a9927c8e7b3b67
Sma-source <rabhi.smain@gmail.com>
2023-06-14 19:14:14 +0200

    destructured props

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 // icons
 import { BsArrowsFullscreen, BsPeople } from "react-icons/bs";
 
-const Room = () => {
+const Room = ({ room }) => {
+  // destructure room
+  const { id, name, image, size, maxPerson, description, price } = room;
   return <div>Room</div>;
 };
 
