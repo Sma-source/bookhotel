d8c2c15c980b9d3b077b87038b4ead532873222d
Sma-source <rabhi.smain@gmail.com>
2023-06-14 19:23:47 +0200

    update div ui

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -10,7 +10,7 @@ const Rooms = () => {
     <section className="py-24">
       <div className="container mx-auto lg:px-0">
         {/* grid */}
-        <div>
+        <div className="grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0">
           {rooms.map((room) => {
             return <Room key={room.id} room={room} />;
           })}
