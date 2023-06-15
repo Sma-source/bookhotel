1308fac7acd2e9ed63ca10c75b44020f129a8a77
Sma-source <rabhi.smain@gmail.com>
2023-06-15 17:36:45 +0200

    make import

--- src/components/AdultsDropdown.jsx
+++ src/components/AdultsDropdown.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+// room context
+import { RoomContext } from "../context/RoomContext";
+// headless ui menu
+import { Menu } from "@headlessui/react";
+// icons
+import { BsChevronDown } from "react-icons/bs";
 
 const AdultsDropdown = () => {
   return <div>AdultsDropdown</div>;
