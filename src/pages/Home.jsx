9965c8b4f929ce017507e8b0c008848ff24b162c
Sma-source <rabhi.smain@gmail.com>
2023-06-15 16:49:51 +0200

    update ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,14 @@ import BookForm from "../components/BookForm";
 const Home = () => {
   return (
     <>
-      <BookForm />
+      <div className="container mx-auto">
+        <div
+          className="bg-accent/20 mt-4 p-4 lg:shadow-xl lg:absolute
+          lg:left-0 lg:right-0 lg:p-0 lg:z-30"
+        >
+          <BookForm />
+        </div>
+      </div>
       <Rooms />
     </>
   );
