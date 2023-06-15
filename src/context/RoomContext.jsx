938a9fbb0594718e37a0a7c854edf6dac4cfb299
Sma-source <rabhi.smain@gmail.com>
2023-06-15 20:04:54 +0200

    add new state

--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -6,9 +6,16 @@ export const RoomContext = createContext();
 
 const RoomProvider = ({ children }) => {
   const [rooms, setRooms] = useState(roomData);
+  const [adults, setAdults] = useState("1 Adult");
+  const [kids, setKids] = useState("0 Kids");
+  const [total, setTotal] = useState(0);
+
+  console.log(`adults ${adults}, kids ${kids}`);
 
   return (
-    <RoomContext.Provider value={{ rooms }}>{children}</RoomContext.Provider>
+    <RoomContext.Provider value={{ rooms, adults, setAdults, kids, setKids }}>
+      {children}
+    </RoomContext.Provider>
   );
 };
 
