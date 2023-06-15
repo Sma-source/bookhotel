3a6baf3affce8dfc49d7dea6ce4fa88d0dc80a78
Sma-source <rabhi.smain@gmail.com>
2023-06-15 16:59:28 +0200

    update ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="container mx-auto relative">
         <div
           className="bg-accent/20 mt-4 p-4 lg:shadow-xl lg:absolute
-          lg:left-0 lg:right-0 lg:p-0 lg:z-30"
+          lg:left-0 lg:right-0 lg:p-0 lg:z-30 lg:-top-12"
         >
           <BookForm />
         </div>
