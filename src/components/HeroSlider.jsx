f7866fd67b3ec95534380d2b1e6171bdb7861fe1
Sma-source <rabhi.smain@gmail.com>
2023-06-16 19:26:55 +0200

    change bg color

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const HeroSlider = () => {
-  return <div className="heroSlider h-[600px] lg:h-[860px]">HeroSlider</div>;
+  return (
+    <div className="heroSlider h-[600px] lg:h-[860px] bg-black">HeroSlider</div>
+  );
 };
 
 export default HeroSlider;
