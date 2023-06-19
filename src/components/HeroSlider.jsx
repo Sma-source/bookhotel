abb9e28c34aff5f6ba8f4b8534f5aa1a25307f87
Sma-source <rabhi.smain@gmail.com>
2023-06-19 18:16:10 +0200

    create ui for slider

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -31,7 +31,43 @@ const slides = [
 
 const HeroSlider = () => {
   return (
-    <div className="heroSlider h-[600px] lg:h-[860px] bg-black">HeroSlider</div>
+    <Swiper
+      modules={[EffectFade, Autoplay]}
+      effect={"fade"}
+      Loop={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      className="heroSlider h-[600px] lg:-h[860px]"
+    >
+      {slides.map((slide, index) => {
+        const { title, bg, btnText } = slide;
+        return (
+          <SwiperSlide
+            key={index}
+            className="h-full relative flex justify-center items-center"
+          >
+            <div className="z-20 text-white text-center">
+              <div className="uppercase font-tertiary tracking-[6px] mb-5">
+                Just Enjoy and relax
+              </div>
+              <h1 className="text-[32px] font-primary uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight">
+                {title}
+              </h1>
+              <button className="btn btn-lg btn-primary mx-auto">
+                {btnText}
+              </button>
+            </div>
+            <div className="absolute top-0 w-full h-full">
+              <img className="object-cover h-full w-full" src={bg} alt="" />
+            </div>
+            {/* overlay */}
+            <div className="absolute w-full h-full bg-black/70"></div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
