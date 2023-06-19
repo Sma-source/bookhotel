8b0bcc1a402dab5ace6c44bc31f825280aa8b5b6
Sma-source <rabhi.smain@gmail.com>
2023-06-19 17:47:36 +0200

    make import

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,4 +1,15 @@
 import React from "react";
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+// import required modules
+import { EffectFade, Autoplay } from "swiper";
+// img
+import Img1 from "../assets/img/heroSlider/1.jpg";
+import Img2 from "../assets/img/heroSlider/2.jpg";
+import Img3 from "../assets/img/heroSlider/3.jpg";
 
 const HeroSlider = () => {
   return (
