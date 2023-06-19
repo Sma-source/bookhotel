386cd64a718394ac10cf478fa65baeb14e838966
Sma-source <rabhi.smain@gmail.com>
2023-06-19 18:26:48 +0200

    add footer ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+// Logo
+import LogoWhite from "../assets/img/logo-white.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto text-white flex flex-col lg:flex-row justify-center items-center lg:justify-between">
+        {/* logo */}
+        <a href="/">
+          <img src={LogoWhite} alt="logo" className="pb-4 lg:pb-0" />
+        </a>
+        Copyright &copy; 2023. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
