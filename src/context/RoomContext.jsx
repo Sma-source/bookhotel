3d4368f8c0aeb14b0b222a44c5c063188010340a
Sma-source <rabhi.smain@gmail.com>
2023-06-19 20:31:29 +0200

    create loading

--- src/context/RoomContext.jsx
+++ src/context/RoomContext.jsx
@@ -9,6 +9,7 @@ const RoomProvider = ({ children }) => {
   const [adults, setAdults] = useState("1 Adult");
   const [kids, setKids] = useState("0 Kids");
   const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   // console.log(`adults ${adults}, kids ${kids}`);
 
@@ -18,17 +19,21 @@ const RoomProvider = ({ children }) => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(total);
+    setLoading(true);
+    // console.log(total);
     // filter rooms based on total (person)
     const newRooms = roomData.filter((room) => {
       return total <= room.maxPerson;
     });
-    setRooms(newRooms);
+    setTimeout(() => {
+      setRooms(newRooms);
+      setLoading(false);
+    }, 3000);
   };
 
   return (
     <RoomContext.Provider
-      value={{ rooms, adults, setAdults, kids, setKids, handleClick }}
+      value={{ rooms, adults, setAdults, kids, setKids, handleClick, loading }}
     >
       {children}
     </RoomContext.Provider>
