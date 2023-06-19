bc085813e903ea80c75cecfa31f75ea3ef66946d
Sma-source <rabhi.smain@gmail.com>
2023-06-19 21:29:58 +0200

    update data description

--- src/data.jsx
+++ src/data.jsx
@@ -32,7 +32,7 @@ export const roomData = [
     id: 1,
     name: "Superior Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Indulge in Spacious Comfort: Immerse yourself in the tranquility of our superior room. With a generous size of 30m², it offers ample space for your utmost relaxation. Designed for the ultimate solo experience, it provides a peaceful retreat where you can unwind and rejuvenate. Treat yourself to the perfect blend of comfort and serenity in our superior room.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -53,7 +53,7 @@ export const roomData = [
     id: 2,
     name: "Signature Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Exquisite Luxury and Space: Experience the Epitome of Elegance in our Signature Room. Spanning an impressive 70m², this refined sanctuary is designed for utmost comfort and style. Perfect for couples or close companions, it offers ample space to relax and unwind together. Discover the perfect harmony of lavishness and intimacy in our Signature Room, where every moment becomes a cherished memory.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -74,7 +74,7 @@ export const roomData = [
     id: 3,
     name: "Deluxe Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Unwind in Comfort and Style: Step into our Deluxe Room, a spacious retreat spanning 50m². Designed with your utmost comfort in mind, this room offers an ideal sanctuary for relaxation. Accommodating up to three people, it provides ample space for you and your loved ones to enjoy a memorable stay. Discover a harmonious blend of comfort, style, and functionality in our Deluxe Room, where every moment is tailored to exceed your expectations.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -95,7 +95,7 @@ export const roomData = [
     id: 4,
     name: "Luxury Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Indulge in Opulent Serenity: Experience the Height of Luxury in our spacious Luxury Room. With its generous size of 50m², this lavish sanctuary offers abundant space for relaxation and rejuvenation. Designed to accommodate up to four people, it is perfect for families or groups seeking unrivaled comfort. Immerse yourself in an exquisite blend of elegance and comfort, where every detail is meticulously crafted to ensure a truly memorable stay in our Luxury Room.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -116,7 +116,7 @@ export const roomData = [
     id: 5,
     name: "Luxury Suite Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Unparalleled Luxury and Space: Discover the epitome of indulgence in our Luxury Suite Room. Spanning an expansive 90m², this lavish sanctuary offers an unrivaled experience in sophistication and comfort. With its impressive capacity for up to five people, it is the perfect choice for larger groups or families seeking the utmost in opulence. Embrace the grandeur and exclusivity of our Luxury Suite Room, where luxury knows no bounds and unforgettable moments await.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -137,7 +137,7 @@ export const roomData = [
     id: 6,
     name: "Deluxe Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Relax in Spacious Comfort: Unwind and recharge in our Deluxe Room, designed for ultimate relaxation. With a generous size of 45m², this well-appointed sanctuary offers ample space for your comfort. Accommodating up to six people, it is perfect for larger families or groups seeking a comfortable and enjoyable stay. Experience the perfect blend of comfort and convenience in our Deluxe Room, where every guest can create lasting memories.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -158,7 +158,7 @@ export const roomData = [
     id: 7,
     name: "Luxury Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea ccusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Unmatched Luxury and Grandeur: Experience opulence like never before in our sprawling Luxury Room. With an impressive size of 84m², this sumptuous haven offers an abundance of space for pure indulgence. Designed to accommodate up to seven guests, it is the perfect choice for larger groups or families seeking unparalleled comfort. Immerse yourself in a world of refined luxury and sophistication in our Luxury Room, where extraordinary experiences await at every turn.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
@@ -179,7 +179,7 @@ export const roomData = [
     id: 8,
     name: "Deluxe Room",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ea placeat eos sed voluptas unde veniam eligendi a. Quaerat molestiae hic omnis temporibus quos consequuntur nam voluptatum ea accusamus, corrupti nostrum eum placeat quibusdam quis beatae quae labore earum architecto aliquid debitis.",
+      "Spacious Comfort for Larger Groups: Unwind in our Deluxe Room, offering an expansive size of 48m². Designed with ample space and comfort in mind, this room is ideal for accommodating larger groups of up to eight people. Embrace the perfect blend of relaxation and convenience, where everyone can enjoy a comfortable and memorable stay. Discover the ideal retreat for your group in our Deluxe Room, where comfort meets versatility in a welcoming ambiance.",
     facilities: [
       { name: "Wifi", icon: <FaWifi /> },
       { name: "Coffee", icon: <FaCoffee /> },
