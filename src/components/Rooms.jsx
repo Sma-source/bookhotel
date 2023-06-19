3b3aac0df547b8b5032db9982b1d66d9bd5ff39b
Sma-source <rabhi.smain@gmail.com>
2023-06-19 20:46:15 +0200

    add loader

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -3,11 +3,19 @@ import React, { useContext } from "react";
 import { RoomContext } from "../context/RoomContext";
 // components
 import Room from "../components/Room";
+// loader
+import { SpinnerCircularFixed } from "spinners-react";
 
 const Rooms = () => {
-  const { rooms } = useContext(RoomContext);
+  const { rooms, loading } = useContext(RoomContext);
   return (
     <section className="py-24">
+      {/* overlay & spinner */}
+      {loading && (
+        <div className="h-screen fixed bottom-0 top-0 bg-black/90 w-full z-50 flex justify-center items-center">
+          <SpinnerCircularFixed color="#967142" size={75} />
+        </div>
+      )}
       <div className="container mx-auto lg:px-0">
         {/* grid */}
         <div className="grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0">
